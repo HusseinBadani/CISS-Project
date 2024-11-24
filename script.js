75eb3e519d8aae7cc8c44fa85b2f72cc83d96377
HusseinBadani <7sinalbadani@gmail.com>
2024-11-24 09:57:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById("convert-button").addEventListener("click", function () {
+    const length = parseFloat(document.getElementById("length").value);
+    const unit = document.getElementById("unit").value;
 
+    if (isNaN(length)) {
+        alert("Please enter a valid number.");
+        return;
+    }
